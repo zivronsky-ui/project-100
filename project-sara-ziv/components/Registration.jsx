e23c1eae2cd98b6b3c409a4980f7606e2824f753
zivronsky-ui <zivronsky@hilma.tech>
2025-11-02 17:55:48 +0200

    start

--- project-sara-ziv/components/Registration.jsx
+++ project-sara-ziv/components/Registration.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useState } from "react";
 function Registration() {
-  function addPerson() {
-    localStorage.setItem("", input.value);
+  const [nameValue, setNameValue] = useState("");
+  const [userArr, setUserArr] = useState([]);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const inputValue = event.target.elements.name.value;
+    localStorage.setItem(inputValue, inputValue);
+    event.target.reset();
+    setNameValue(inputValue);
+    console.log(inputValue);
+    setUserArr(
+      userArr.push({ name: inputValue, score: 0, status: "wait for your turn" })
+    );
+    // userArr.map((user)=>{})
+    function passTheTurn() {
+      // פונקציה שאמורה להעבירר את התור כאשר מישהו לוחץ ל כפתור מסויים ואז מוסיפים את זה לתוך הפונקציות שמחכות לONCLICK
+      userArr[currentIndex + 1].status = "your turn";
+    }
+    return (
+      <>
+        <form onSubmit={handleSubmit}>
+          <input name="name" />
+          <button type="submit">Add person</button>
+        </form>
+      </>
+    );
   }
-  return (
-    <>
-      <input type="text" id="add" />
-      <button onClick={addPerson}>Add User</button>
-    </>
-  );
+  export default Registration;
 }
-export default Registration;
+// status =

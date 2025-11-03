4e29011d03582fe88c428b622f6ed9dba053a289
sarakahan1 <sarakahan@hilma.tech>
2025-11-03 16:24:55 +0200

    j

--- project-sara-ziv/components/Registration.jsx
+++ project-sara-ziv/components/Registration.jsx
@@ -1,41 +1,48 @@
-import React from "react";
-import { useState } from "react";
-import BasicGame from "./BasicGame";
-function Registration() {
-  const [nameValue, setNameValue] = useState("");
-  const [isStart, setIsStart] = useState(false);
-  const [userArr, setUserArr] = useState([]);
-  function handleSubmit(event) {
-    event.preventDefault();
-    const inputValue = event.target.elements.name.value;
-    localStorage.setItem(inputValue, 0);
-    event.target.reset();
-    setNameValue(inputValue);
-    setUserArr([...userArr, inputValue]);
-    console.log(inputValue);
+// import React from "react";
+// import { useState } from "react";
+// // import BasicGame from "./BasicGame";
+// import Boards from "./Boards";
+// function Registration() {
+//   const [nameValue, setNameValue] = useState("");
+//   const [isStart, setIsStart] = useState(false);
+//   const [userArr, setUserArr] = useState([]);
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const inputValue = event.target.elements.name.value;
+//     localStorage.setItem(inputValue, 0);
+//     event.target.reset();
+//     setNameValue(inputValue);
+//     setUserArr([...userArr, inputValue]);
+//     console.log(inputValue);
 
-    console.log(userArr);
-    console.log(userArr.length);
-  }
-  function startGame() {
-    console.log("start");
-    setIsStart(true);
-  }
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input name="name" />
-        <button type="submit">Add person</button>
-      </form>
-      <button onClick={startGame}>Start</button>
-      {isStart ? (
-        <div>
-          <BasicGame />
-        </div>
-      ) : null}
-      {/* <a src="/BasicGame">start</a> */}
-    </>
-  );
-}
-// status =
-export default Registration;
+//     console.log(userArr);
+//     console.log(userArr.length);
+//   }
+//   function startGame() {
+//     console.log("start");
+//     setIsStart(true);
+//   }
+
+//   // const [turn, setTurn] = useState(0);
+//   // const changeTurn = () => {
+//   //   setTurn((prevTurn) => (prevTurn + 1) % 3);
+//   // };
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input name="name" />
+//         <button type="submit">Add person</button>
+//       </form>
+//       <button onClick={startGame}>Start</button>
+//       {isStart ? (
+//         <div>
+//           <Boards />
+//         </div>
+//       ) : null}
+//       {/* <a src="/BasicGame">start</a> */}
+//     </>
+//   );
+// }
+// // status =
+// export default Registration;
--- project-sara-ziv/src/App.jsx
+++ project-sara-ziv/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import "./App.css";
-import Registration from "../components/Registration";
-// import BasicGame from "../components/BasicGame";
+// import Registration from "../components/Registration";
+import BasicGame from "../components/BasicGame";
 import Boards from "../components/Boards";
+// import AddPerson from "../components/AddPerson";
 
 function App() {
   return (
     <>
-      <Registration />
-      {/* <BasicGame /> */}
-      <Boards />
+      {/* <Registration /> */}
+      / <Boards />
+      {/* <AddPerson /> */}
     </>
   );
 }
